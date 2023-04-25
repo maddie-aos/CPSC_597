f47740304fa86ccb7a50c96e731b814bea4dc1f2
Maddie Smith <54409681+maddie-aos@users.noreply.github.com>
2023-04-24 21:41:41 -0700

    commit

--- deploy_webapp/app.py
+++ deploy_webapp/app.py
@@ -251,11 +251,11 @@ def predict():
     row=row.values
     col=col.values
     
-    prediction_array=np.save('deploy_webapp/predictions/prediction_array.npy',input_X)
-    prediction_pandas=row_col.to_csv('deploy_webapp/predictions/prediction_row_col.csv')
+    prediction_array=np.save('deploy_webapp/predictions/csor_future_prediction_array.npy',input_X)
+    prediction_pandas=row_col.to_csv('deploy_webapp/predictions/csor_future_prediction_row_col.csv')
 
     #predicting outcome
-    input_X=np.load('deploy_webapp/predictions/prediction_array.npy')
+    input_X=np.load('deploy_webapp/predictions/csor_future_prediction_array.npy')
     df=pd.DataFrame(input_X)
 
     #create copy of band to later subset values in
@@ -285,21 +285,521 @@ def predict():
 def eng_mor_fut_pred():
     return render_template("eng_mor_fut_pred.html")
 
+@app.route("/eng_mor_fut_pred", methods = ['POST'])
+def predict():
+    #taking in user input, making a dataframe
+    lat = request.form.get('latitudechange')
+    latitude = float(lat)
+    lon = request.form.get('longitudechange')
+    longitude = float(lon)
+    items = {"deci_lat": [latitude], "deci_lon": [longitude]}
+    df = pd.DataFrame(items)
+
+    #reading raster and extracting values 
+    inRas=gdal.Open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif')
+    myarray=inRas.ReadAsArray()
+
+    len_pd=np.arange(len(df))
+    lon=df["deci_lon"]
+    lat=df["deci_lat"]
+    lon=lon.values
+    lat=lat.values
+    
+    
+    row=[]
+    col=[]
+
+
+    src=rasterio.open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif', crs= 'espg: 4326')
+    for i in len_pd:
+        row_n, col_n = src.index(lon[i], lat[i])# spatial --> image coordinates
+        row.append(row_n)
+        col.append(col_n)
+    
+    mean_std=pd.read_csv('data/modified_data/stacked_bio_oracle_future/future_env_bio_mean_std.txt',sep="\t")
+    mean_std=mean_std.to_numpy()
+
+    X=[]
+    for j in range(0,9):
+        print(j)
+        band=myarray[j]
+        x=[]
+        
+        for i in range(0,len(row)):
+            value= band[row[i],col[i]]
+            if value <-1000:
+                x.append(value)
+            else:
+                value = ((value - mean_std.item((j,1))) / mean_std.item((j,2))) # scale values
+                x.append(value)
+        X.append(x)
+
+    X.append(row)
+    X.append(col)
+
+    X =np.array([np.array(xi) for xi in X])
+    
+    df=pd.DataFrame(X)
+    df=df.T
+    
+    #drop any rows with no-data values
+    df=df.dropna(axis=0, how='any')
+    input_X=df.loc[:,0:8]
+    
+    row=df[9]
+    col=df[10]
+    
+    row_col=pd.DataFrame({"row":row,"col":col})
+   
+    input_X=input_X.values
+    
+    #convert rows and col indices back to array
+    row=row.values
+    col=col.values
+    
+    prediction_array=np.save('deploy_webapp/predictions/emor_future_prediction_array.npy',input_X)
+    prediction_pandas=row_col.to_csv('deploy_webapp/predictions/emor_future_prediction_row_col.csv')
+
+    #predicting outcome
+    input_X=np.load('deploy_webapp/predictions/emor_future_prediction_array.npy')
+    df=pd.DataFrame(input_X)
+
+    #create copy of band to later subset values in
+    new_band=myarray[1].copy()
+    new_band.shape
+
+    new_values = eng_mor_model_future.predict(x=input_X,verbose=0) ###predict output value
+
+    ##take the prob. of presence (new_value.item((0,1))) and put into numpy array
+    new_band_values=[]
+   
+    for i in new_values:
+        new_value=i[1]
+        new_band_values.append(new_value)
+    new_band_values=np.array(new_band_values)
+
+    my_string = "Predicted Probability: "
+    resultdf = pd.DataFrame(new_band_values, columns=['result'])
+    result_value = resultdf['result'].values[0]
+    result = my_string + str(result_value)
+
+    return result
+
 @app.route("/par_cal_fut_pred")
 def par_cal__fut_pred():
     return render_template("par_cal_fut_pred.html")
+
+@app.route("/par_cal_fut_pred", methods = ['POST'])
+def predict():
+    #taking in user input, making a dataframe
+    lat = request.form.get('latitudechange')
+    latitude = float(lat)
+    lon = request.form.get('longitudechange')
+    longitude = float(lon)
+    items = {"deci_lat": [latitude], "deci_lon": [longitude]}
+    df = pd.DataFrame(items)
+
+    #reading raster and extracting values 
+    inRas=gdal.Open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif')
+    myarray=inRas.ReadAsArray()
+
+    len_pd=np.arange(len(df))
+    lon=df["deci_lon"]
+    lat=df["deci_lat"]
+    lon=lon.values
+    lat=lat.values
+    
+    
+    row=[]
+    col=[]
+
+
+    src=rasterio.open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif', crs= 'espg: 4326')
+    for i in len_pd:
+        row_n, col_n = src.index(lon[i], lat[i])# spatial --> image coordinates
+        row.append(row_n)
+        col.append(col_n)
+    
+    mean_std=pd.read_csv('data/modified_data/stacked_bio_oracle_future/future_env_bio_mean_std.txt',sep="\t")
+    mean_std=mean_std.to_numpy()
+
+    X=[]
+    for j in range(0,9):
+        print(j)
+        band=myarray[j]
+        x=[]
+        
+        for i in range(0,len(row)):
+            value= band[row[i],col[i]]
+            if value <-1000:
+                x.append(value)
+            else:
+                value = ((value - mean_std.item((j,1))) / mean_std.item((j,2))) # scale values
+                x.append(value)
+        X.append(x)
+
+    X.append(row)
+    X.append(col)
+
+    X =np.array([np.array(xi) for xi in X])
+    
+    df=pd.DataFrame(X)
+    df=df.T
+    
+    #drop any rows with no-data values
+    df=df.dropna(axis=0, how='any')
+    input_X=df.loc[:,0:8]
+    
+    row=df[9]
+    col=df[10]
+    
+    row_col=pd.DataFrame({"row":row,"col":col})
+   
+    input_X=input_X.values
+    
+    #convert rows and col indices back to array
+    row=row.values
+    col=col.values
+    
+    prediction_array=np.save('deploy_webapp/predictions/pcal_future_prediction_array.npy',input_X)
+    prediction_pandas=row_col.to_csv('deploy_webapp/predictions/pcal_future_prediction_row_col.csv')
+
+    #predicting outcome
+    input_X=np.load('deploy_webapp/predictions/pcal_future_prediction_array.npy')
+    df=pd.DataFrame(input_X)
+
+    #create copy of band to later subset values in
+    new_band=myarray[1].copy()
+    new_band.shape
+
+    new_values = par_cal_model_future.predict(x=input_X,verbose=0) ###predict output value
+
+    ##take the prob. of presence (new_value.item((0,1))) and put into numpy array
+    new_band_values=[]
+   
+    for i in new_values:
+        new_value=i[1]
+        new_band_values.append(new_value)
+    new_band_values=np.array(new_band_values)
+
+    my_string = "Predicted Probability: "
+    resultdf = pd.DataFrame(new_band_values, columns=['result'])
+    result_value = resultdf['result'].values[0]
+    result = my_string + str(result_value)
+
+    return result
 
 @app.route("/sco_jap_fut_pred")
 def sco_jap_fut_pred():
     return render_template("sco_jap_fut_pred.html")
 
+@app.route("/sco_jap_fut_pred", methods = ['POST'])
+def predict():
+    #taking in user input, making a dataframe
+    lat = request.form.get('latitudechange')
+    latitude = float(lat)
+    lon = request.form.get('longitudechange')
+    longitude = float(lon)
+    items = {"deci_lat": [latitude], "deci_lon": [longitude]}
+    df = pd.DataFrame(items)
+
+    #reading raster and extracting values 
+    inRas=gdal.Open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif')
+    myarray=inRas.ReadAsArray()
+
+    len_pd=np.arange(len(df))
+    lon=df["deci_lon"]
+    lat=df["deci_lat"]
+    lon=lon.values
+    lat=lat.values
+    
+    
+    row=[]
+    col=[]
+
+
+    src=rasterio.open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif', crs= 'espg: 4326')
+    for i in len_pd:
+        row_n, col_n = src.index(lon[i], lat[i])# spatial --> image coordinates
+        row.append(row_n)
+        col.append(col_n)
+    
+    mean_std=pd.read_csv('data/modified_data/stacked_bio_oracle_future/future_env_bio_mean_std.txt',sep="\t")
+    mean_std=mean_std.to_numpy()
+
+    X=[]
+    for j in range(0,9):
+        print(j)
+        band=myarray[j]
+        x=[]
+        
+        for i in range(0,len(row)):
+            value= band[row[i],col[i]]
+            if value <-1000:
+                x.append(value)
+            else:
+                value = ((value - mean_std.item((j,1))) / mean_std.item((j,2))) # scale values
+                x.append(value)
+        X.append(x)
+
+    X.append(row)
+    X.append(col)
+
+    X =np.array([np.array(xi) for xi in X])
+    
+    df=pd.DataFrame(X)
+    df=df.T
+    
+    #drop any rows with no-data values
+    df=df.dropna(axis=0, how='any')
+    input_X=df.loc[:,0:8]
+    
+    row=df[9]
+    col=df[10]
+    
+    row_col=pd.DataFrame({"row":row,"col":col})
+   
+    input_X=input_X.values
+    
+    #convert rows and col indices back to array
+    row=row.values
+    col=col.values
+    
+    prediction_array=np.save('deploy_webapp/predictions/sjap_future_prediction_array.npy',input_X)
+    prediction_pandas=row_col.to_csv('deploy_webapp/predictions/sjap_future_prediction_row_col.csv')
+
+    #predicting outcome
+    input_X=np.load('deploy_webapp/predictions/sjap_future_prediction_array.npy')
+    df=pd.DataFrame(input_X)
+
+    #create copy of band to later subset values in
+    new_band=myarray[1].copy()
+    new_band.shape
+
+    new_values = sco_jap_model_future.predict(x=input_X,verbose=0) ###predict output value
+
+    ##take the prob. of presence (new_value.item((0,1))) and put into numpy array
+    new_band_values=[]
+   
+    for i in new_values:
+        new_value=i[1]
+        new_band_values.append(new_value)
+    new_band_values=np.array(new_band_values)
+
+    my_string = "Predicted Probability: "
+    resultdf = pd.DataFrame(new_band_values, columns=['result'])
+    result_value = resultdf['result'].values[0]
+    result = my_string + str(result_value)
+
+    return result
+
 @app.route("/thu_ala_fut_pred")
 def thu_ala_fut_pred():
     return render_template("thu_ala_fut_pred.html")
 
+@app.route("/thu_ala_fut_pred", methods = ['POST'])
+def predict():
+    #taking in user input, making a dataframe
+    lat = request.form.get('latitudechange')
+    latitude = float(lat)
+    lon = request.form.get('longitudechange')
+    longitude = float(lon)
+    items = {"deci_lat": [latitude], "deci_lon": [longitude]}
+    df = pd.DataFrame(items)
+
+    #reading raster and extracting values 
+    inRas=gdal.Open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif')
+    myarray=inRas.ReadAsArray()
+
+    len_pd=np.arange(len(df))
+    lon=df["deci_lon"]
+    lat=df["deci_lat"]
+    lon=lon.values
+    lat=lat.values
+    
+    
+    row=[]
+    col=[]
+
+
+    src=rasterio.open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif', crs= 'espg: 4326')
+    for i in len_pd:
+        row_n, col_n = src.index(lon[i], lat[i])# spatial --> image coordinates
+        row.append(row_n)
+        col.append(col_n)
+    
+    mean_std=pd.read_csv('data/modified_data/stacked_bio_oracle_future/future_env_bio_mean_std.txt',sep="\t")
+    mean_std=mean_std.to_numpy()
+
+    X=[]
+    for j in range(0,9):
+        print(j)
+        band=myarray[j]
+        x=[]
+        
+        for i in range(0,len(row)):
+            value= band[row[i],col[i]]
+            if value <-1000:
+                x.append(value)
+            else:
+                value = ((value - mean_std.item((j,1))) / mean_std.item((j,2))) # scale values
+                x.append(value)
+        X.append(x)
+
+    X.append(row)
+    X.append(col)
+
+    X =np.array([np.array(xi) for xi in X])
+    
+    df=pd.DataFrame(X)
+    df=df.T
+    
+    #drop any rows with no-data values
+    df=df.dropna(axis=0, how='any')
+    input_X=df.loc[:,0:8]
+    
+    row=df[9]
+    col=df[10]
+    
+    row_col=pd.DataFrame({"row":row,"col":col})
+   
+    input_X=input_X.values
+    
+    #convert rows and col indices back to array
+    row=row.values
+    col=col.values
+    
+    prediction_array=np.save('deploy_webapp/predictions/tala_future_prediction_array.npy',input_X)
+    prediction_pandas=row_col.to_csv('deploy_webapp/predictions/tala_future_prediction_row_col.csv')
+
+    #predicting outcome
+    input_X=np.load('deploy_webapp/predictions/tala_future_prediction_array.npy')
+    df=pd.DataFrame(input_X)
+
+    #create copy of band to later subset values in
+    new_band=myarray[1].copy()
+    new_band.shape
+
+    new_values = thu_ala_model_future.predict(x=input_X,verbose=0) ###predict output value
+
+    ##take the prob. of presence (new_value.item((0,1))) and put into numpy array
+    new_band_values=[]
+   
+    for i in new_values:
+        new_value=i[1]
+        new_band_values.append(new_value)
+    new_band_values=np.array(new_band_values)
+
+    my_string = "Predicted Probability: "
+    resultdf = pd.DataFrame(new_band_values, columns=['result'])
+    result_value = resultdf['result'].values[0]
+    result = my_string + str(result_value)
+
+    return result
+
 @app.route("/xip_gla_fut_pred")
 def xip_gla_fut_pred():
     return render_template("xip_gla_fut_pred.html")
+
+@app.route("/xip_gla_fut_pred", methods = ['POST'])
+def predict():
+    #taking in user input, making a dataframe
+    lat = request.form.get('latitudechange')
+    latitude = float(lat)
+    lon = request.form.get('longitudechange')
+    longitude = float(lon)
+    items = {"deci_lat": [latitude], "deci_lon": [longitude]}
+    df = pd.DataFrame(items)
+
+    #reading raster and extracting values 
+    inRas=gdal.Open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif')
+    myarray=inRas.ReadAsArray()
+
+    len_pd=np.arange(len(df))
+    lon=df["deci_lon"]
+    lat=df["deci_lat"]
+    lon=lon.values
+    lat=lat.values
+    
+    
+    row=[]
+    col=[]
+
+
+    src=rasterio.open('data/modified_data/stacked_bio_oracle_future/bio_oracle_future_stacked.tif', crs= 'espg: 4326')
+    for i in len_pd:
+        row_n, col_n = src.index(lon[i], lat[i])# spatial --> image coordinates
+        row.append(row_n)
+        col.append(col_n)
+    
+    mean_std=pd.read_csv('data/modified_data/stacked_bio_oracle_future/future_env_bio_mean_std.txt',sep="\t")
+    mean_std=mean_std.to_numpy()
+
+    X=[]
+    for j in range(0,9):
+        print(j)
+        band=myarray[j]
+        x=[]
+        
+        for i in range(0,len(row)):
+            value= band[row[i],col[i]]
+            if value <-1000:
+                x.append(value)
+            else:
+                value = ((value - mean_std.item((j,1))) / mean_std.item((j,2))) # scale values
+                x.append(value)
+        X.append(x)
+
+    X.append(row)
+    X.append(col)
+
+    X =np.array([np.array(xi) for xi in X])
+    
+    df=pd.DataFrame(X)
+    df=df.T
+    
+    #drop any rows with no-data values
+    df=df.dropna(axis=0, how='any')
+    input_X=df.loc[:,0:8]
+    
+    row=df[9]
+    col=df[10]
+    
+    row_col=pd.DataFrame({"row":row,"col":col})
+   
+    input_X=input_X.values
+    
+    #convert rows and col indices back to array
+    row=row.values
+    col=col.values
+    
+    prediction_array=np.save('deploy_webapp/predictions/xgla_future_prediction_array.npy',input_X)
+    prediction_pandas=row_col.to_csv('deploy_webapp/predictions/xgla_future_prediction_row_col.csv')
+
+    #predicting outcome
+    input_X=np.load('deploy_webapp/predictions/xgla_future_prediction_array.npy')
+    df=pd.DataFrame(input_X)
+
+    #create copy of band to later subset values in
+    new_band=myarray[1].copy()
+    new_band.shape
+
+    new_values = xip_gla_model_future.predict(x=input_X,verbose=0) ###predict output value
+
+    ##take the prob. of presence (new_value.item((0,1))) and put into numpy array
+    new_band_values=[]
+   
+    for i in new_values:
+        new_value=i[1]
+        new_band_values.append(new_value)
+    new_band_values=np.array(new_band_values)
+
+    my_string = "Predicted Probability: "
+    resultdf = pd.DataFrame(new_band_values, columns=['result'])
+    result_value = resultdf['result'].values[0]
+    result = my_string + str(result_value)
+
+    return result
 
 
 #Loading machine learning models
